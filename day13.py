f0db033b0db27b00cf59f874c45d80aa25cb06fd
Justin Gray <justin.s.gray@gmail.com>
2017-12-14 23:46:50 -0500

    fixing day13 so its not terribly slow. Just in case I need it in a later day

--- day13.py
+++ day13.py
@@ -9,68 +9,34 @@ def parse_input(input):
 
     return data
 
-def build_firewall(data): 
-    fw = [None,]
-    scan_dir = {}
-    for depth, scan_range in data: 
 
-        n_fw = len(fw)
-        if n_fw <= depth: 
-            n_new = depth - n_fw + 1
-            fw += n_new*[None,]
-        fw[depth] = scan_range*[False,]
-        fw[depth][0] = True
-        scan_dir[depth] = 1
-    return fw, scan_dir
-
-def increment_scanners(fw, scan_dir):
-    for i, scanner in enumerate(fw): 
-        if scanner is not None: 
-            scan_range = len(scanner)
-            i_scanner = scanner.index(True)
-            scanner[i_scanner] = False
-            
-            if i_scanner == 0: 
-                dir = scan_dir[i] = 1
-            elif i_scanner == scan_range - 1: 
-                dir = scan_dir[i] = -1
-            else: 
-                dir = scan_dir[i]
-
-            scanner[i_scanner+dir] = True
-
-def check_caught(depth, fw): 
-    scanner = fw[depth]
-    if scanner is not None: 
-        if scanner[0]: 
-            return depth * len(scanner), True
+def check_caught(depth, s_range, delay):
+    if (depth+delay) % ((s_range-1)*2) == 0: 
+        return depth*s_range, True
     return 0, False
 
-def walk(fw, scan_dir, fast_quit=False): 
-    n_fw = len(fw)
+def walk(data, fast_quit=False, delay=0): 
+    
     caughts = []
     any_caught = False
-    for depth in range(n_fw): 
-        severity, check = check_caught(depth, fw)
+    for depth, s_range in data: 
+        severity, check = check_caught(depth, s_range, delay)
         if fast_quit and check: 
             return [1], True
         caughts.append(severity)
 
         any_caught = any_caught or check
-        increment_scanners(fw, scan_dir)
 
     return caughts, any_caught
 
-def delay(fw, scan_dir): 
+def find_delay(data): 
 
-    caughts, check = walk(copy.deepcopy(fw), copy.deepcopy(scan_dir), True)
     delay = 0
-    while check: 
-        increment_scanners(fw, scan_dir)
-        caughts, check = walk(copy.deepcopy(fw), copy.deepcopy(scan_dir), True)
-        # print('foo', delay, check, sum(caughts)) 
-
-        delay += 1 
+    while True: 
+        severity, check = walk(data, fast_quit=True, delay=delay)
+        if not check: 
+            break 
+        delay += 1
         
     return delay
 
@@ -86,13 +52,11 @@ if __name__ == "__main__":
     # fw, scan_dir = build_firewall(test1_data)
     # print('test1 struct', fw, scan_dir)
 
-    # caughts, check = walk(fw, scan_dir)
-    # print('test1 ', caughts, sum(caughts))
-    
-    fw, scan_dir = build_firewall(test1_data)
+    caughts, check = walk(test1_data)
+    print('test1 ', caughts, sum(caughts))
+
     # print('test2 start check', fw)
-    print("test2", delay(fw, scan_dir))
-    # exit()
+    print("test2", find_delay(test1_data))
     real_input = """0: 3
 1: 2
 2: 5
@@ -139,12 +103,11 @@ if __name__ == "__main__":
 98: 18"""
 
     real1_data = parse_input(real_input)
-    fw, scan_dir = build_firewall(real1_data)
-    caughts, check = walk(fw, scan_dir)
+    caughts, check = walk(real1_data)
     print('real1', sum(caughts))
 
-    fw, scan_dir = build_firewall(real1_data)
-    print("real2", delay(fw, scan_dir))
+    
+    print("real2", find_delay(real1_data))
 
 
 
